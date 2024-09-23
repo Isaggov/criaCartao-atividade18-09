846208a5d34000d5ec0f233ec626f259bfcd51f0
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-09-23 11:39:36 -0300

    gnbxfgn

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // let nomeUsuario='Isadora';
 // alert (nomeUsuario) ;
-function soma(num1,num2){
-   return num1+num2;
+function soma(num1,num2,num3,num4){
+   return num1+num2+num3+num4;
 }
 console.log(soma(1,2));
 
