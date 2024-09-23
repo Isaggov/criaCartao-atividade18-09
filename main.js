d24f873b4f5e49960982b58c30ef244d6f0c1466
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-09-23 11:43:12 -0300

    stdbh

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 // let nomeUsuario='Isadora';
 // alert (nomeUsuario) ;
 function soma(num1,num2,){
-   return num1*num2;
+   return num1+num2;
 }
 console.log(soma(1,2));
 
-console.log(soma (6,4));
+
+function areaQuadrado(lado){
+    return lado**2
+
+}
+console.log(areaQuadrado(6));
