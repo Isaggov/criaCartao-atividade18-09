ec51c7a032ee9da270b5443990457ad9bd8ded29
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-09-30 12:00:16 -0300

    gfjhfh

--- main.js
+++ main.js
@@ -28,11 +28,16 @@
 //      return(num1+num2+num3)/3;
 //  }
 // document.write(media( MedirMedia1,MedirMedia2,MedirMedia3 ));
-let estados=['Acre','Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará','Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins',]
-let i= 0;
-while( i<estados.length){
-console.log(i); 
-console.log(estados[i]);
- i++;}
+// let estados=['Acre','Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará','Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins',]
+// let i= 0;
+// while( i<estados.length){
+// console.log(i); 
+// console.log(estados[i]);
+//  i++;}
+
+ for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+ }
 
  
