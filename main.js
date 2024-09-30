c179cc7ea43ea1a1e87ba2e3b32c6332895940c8
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-09-30 11:55:11 -0300

    dfxhdxf

--- main.js
+++ main.js
@@ -30,9 +30,9 @@
 // document.write(media( MedirMedia1,MedirMedia2,MedirMedia3 ));
 let estados=['Acre','Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará','Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins',]
 let i= 0;
-while( i<10){
+while( i<estados.length){
 console.log(i); 
 console.log(estados[;]);
- i=i+1;}
+ i++;}
 
  
