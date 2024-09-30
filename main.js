ad575ef4e1522a8baace120d041d7abde0857b6d
Isaggouv <142433145+Isaggov@users.noreply.github.com>
2024-09-30 11:19:54 -0300

    xgdfg

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 // let nomeUsuario='Isadora';
 // alert (nomeUsuario) ;
-function soma(num1,num2,){
-   return num1+num2;
-}
-console.log(soma(1,2));
+// function soma(num1,num2,){
+//    return num1+num2;
+// }
+// console.log(soma(1,2));
 
 
-function areaQuadrado(lado){
-    return lado**2
+// function areaQuadrado(lado){
+//     return lado**2
 
-}
-console.log(areaQuadrado(6));
+// }
+// console.log(areaQuadrado(6));
 
-let areaTriangulo=function(base,altura){
-   return (base*altura)/2;
-}
-console.log(areaTriangulo(3,4));
+// let areaTriangulo=function(base,altura){
+//    return (base*altura)/2;
+// }
+// console.log(areaTriangulo(3,4));
 
-let MedirMedia1=parseInt(prompt('nota primeiro trimestre'));
-
-
-let MedirMedia2=parseInt(prompt('nota segundo trimestre'));
+// let MedirMedia1=parseInt(prompt('nota primeiro trimestre'));
 
 
-let MedirMedia3=parseInt(prompt('nota terceiro trimestre'));
-function media(num1,num2,num3){
-     return(num1+num2+num3)/3;
- }
-document.write(media( MedirMedia1,MedirMedia2,MedirMedia3 ));
+// let MedirMedia2=parseInt(prompt('nota segundo trimestre'));
+
+
+// let MedirMedia3=parseInt(prompt('nota terceiro trimestre'));
+// function media(num1,num2,num3){
+//      return(num1+num2+num3)/3;
+//  }
+// document.write(media( MedirMedia1,MedirMedia2,MedirMedia3 ));
+let estados=['Acre','Alagoas', 'Amapá', 'Amazonas', 'Bahia','Ceará','Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins',]
